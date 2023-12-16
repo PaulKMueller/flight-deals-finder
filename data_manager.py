b76c65ec907ec9bb128371e1402a767d2387a67b
PaulKMueller <p.k.m@outlook.de>
2023-12-16 11:42:17 +0100

    [refactor] Encapsulated Functionality in data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,42 @@
+import yaml
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+with open("config.yml") as file:
+    config = yaml.safe_load(file)
+    SHEETY_USER =  config["SHEETY_USER"]
+    SHEETY_PASSWORD = config["SHEETY_PASSWORD"]
+    AUTH_SHEETY = config["AUTH_SHEETY"]
+    SHEETY_ENDPOINT = config["SHEETY_ENDPOINT"]
+    KIWI_API_KEY = config["KIWI_API_KEY"]
+    KIWI_ENDPOINT = config["KIWI_ENDPOINT"]
+    HOMETOWN_IATA = config["HOMETOWN_IATA"]
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.basic_auth = HTTPBasicAuth(SHEETY_USER, SHEETY_PASSWORD)
+        self.headers = {
+            "Authorization": AUTH_SHEETY
+        }
+        self.sheet_data = self.get_sheet_data()
+
+    def get_sheet_data(self):
+        return requests.get(SHEETY_ENDPOINT, headers=self.headers, auth=self.basic_auth).json()["prices"]
+    
+    def fill_in_iata(self):
+        for entry in self.sheet_data:
+            body = {
+                "price":{
+                    "iataCode": self.getIataCodeForCity(entry['city'])
+                    }
+                }
+            response = requests.put(SHEETY_ENDPOINT + f"/{entry['id']}", json=body, headers=self.headers, auth=self.basic_auth)
+
+    @staticmethod
+    def getIataCodeForCity(city: str) -> str:
+        with open("parsed.json", encoding="utf-8") as file:
+            iata_dict = json.load(file)
+            iata_dict = dict((y, x) for x, y in iata_dict.items())
+        return iata_dict[city]
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 import yaml
+from data_manager import DataManager
 
 with open("config.yml") as file:
     config = yaml.safe_load(file)
@@ -17,34 +18,9 @@ with open("config.yml") as file:
 
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
-basic_auth = HTTPBasicAuth(SHEETY_USER, SHEETY_PASSWORD)
+data_manager = DataManager()
 
-headers = {
-    "Authorization": AUTH_SHEETY
-}
-
-# sheet_data = requests.get(SHEETY_ENDPOINT, headers=headers, auth=basic_auth).json()["prices"]
-
-# def getIataCodeForCity(city: str) -> str:
-#     with open("parsed.json", encoding="utf-8") as file:
-#         iata_dict = json.load(file)
-#         iata_dict = dict((y, x) for x, y in iata_dict.items())
-#     return iata_dict[city]
-
-# for entry in sheet_data:
-#     body = {
-#         "price":{
-#             "iataCode": getIataCodeForCity(entry['city'])
-#             }
-#         }
-#     response = requests.put(SHEETY_ENDPOINT + f"/{entry['id']}", json=body, headers=headers, auth=basic_auth)
-#     print(response)
-# print(sheet_data)
-
-sheet_data = requests.get(SHEETY_ENDPOINT, headers=headers, auth=basic_auth).json()["prices"]
-print(sheet_data)
-
-for entry in sheet_data:
+for entry in data_manager.sheet_data:
     today = dt.datetime.today()
     today_string = today.strftime("%d/%m/%Y")
     till_day = today + dt.timedelta(weeks=26)
